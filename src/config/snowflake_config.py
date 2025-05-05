55838251af69cd56974a7c9c348401d6d662ea55
andresariasme <andres.ariasm@upb.edu.co>
2025-05-04 22:51:34 -0500

    security fix

--- src/config/snowflake_config.py
+++
@@ -1,9 +0,0 @@
-SNOWFLAKE_CONFIG = {
-    'account': 'idlkunk-ax51793',  
-    'user': 'aarias04',
-    'password': 'MinisterArias04',
-    'warehouse': 'COMPUTE_WH',
-    'database': 'GITHUB_DATA',
-    'schema': 'BRONZE',
-    'role': 'ACCOUNTADMIN'  
-}
